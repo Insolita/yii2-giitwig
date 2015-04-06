05e533ab40a1750091cc2bb48049cef6012f28aa
insolita <donnainsolita@gmail.com>
2015-04-06 19:55:23 +0800

    finish, ListView support

---
+++ crud/default/views/_listviewitems.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $generator insolita\giitwig\crud\Generator */
+
+$urlParams = $generator->generateTwigUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+
+echo '{{ html.a(model.'.$nameAttribute.',["view", '.$urlParams.' ]) |raw}}';
+
